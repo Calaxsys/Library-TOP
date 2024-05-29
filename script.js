efd368b498d3acba3e18562607ba7d44a4c0d44a
Chris <ffson1999@yahoo.com>
2024-05-29 12:53:52 -0400

    Feat added: - Delete button to each book that is displayed

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
+Book.prototype.toggleRead = function() {
+  this.read = !this.read;
+}
+
 //Functions
 //-------------------------------------------------------
 //Function to add books to the library
@@ -24,16 +28,19 @@ function addBookToLibrary() {
   displayBooks();
 }
 
+//Function to hide the book-cards and show the form
 function openForm() {
   document.getElementById("add-book-form").style.display = "block";
   document.getElementById("book-cards").style.display = "none";
 }
 
+//Function to hide the form and show the book-cards
 function closeForm() {
   document.getElementById("add-book-form").style.display = "none";
   document.getElementById("book-cards").style.display = "flex";
 }
 
+//Function to add the books to the html from the myLibrary array
 function displayBooks() {
   const libraryDisplay = document.getElementById("book-cards");
   libraryDisplay.innerHTML = "";
@@ -45,12 +52,25 @@ function displayBooks() {
         <span class="title-txt">${book.title}</span>
         <span class="author-txt">by: ${book.author}</span>
         <span class="pages-txt">${book.pages}</span>
-        <span>${book.read ? "Read" : "Not Read"}</span>
+        <button class="toggle-read-btn" onclick="toggleRead(${i})">${book.read ? "Read" : "Not Read"}</button>
+        <button class="remove-book-btn">Delete</button>
       </div>
       `;
     libraryDisplay.appendChild(bookCardDisplay);
     document.getElementById("book-form").reset();
   }
+}
+
+//Function to toggle the read button on the book card
+function toggleRead(index) {
+  myLibrary[index].toggleRead();
+  displayBooks();
+}
+
+//Function to delete a book when delete button is clicked
+function deleteBook(index) {
+  myLibrary.splice(index, 1)
+  displayBooks();
 }
 
 //Event Listeners
@@ -62,3 +82,4 @@ document.getElementById("book-form").addEventListener("submit", (event) => {
   addBookToLibrary();
   closeForm();
 });
+
