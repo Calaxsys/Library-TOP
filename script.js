5a5a2dee50df36ded2610aaa2181d41fbf55cb1f
Chris <ffson1999@yahoo.com>
2024-05-28 20:58:55 -0400

    Added form element and hiding and displaying on button clicks

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ function addBookToLibrary() {
   let userInput = prompt("enter a book")
   myLibrary.push(userInput)
 }
+
+
+function openForm() {
+  document.getElementById("add-book-form").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("add-book-form").style.display = "none";
+}
