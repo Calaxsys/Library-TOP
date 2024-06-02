69d9cfb03a73b61b7c2f9f27ab51879eceb28943
Chris <ffson1999@yahoo.com>
2024-06-01 21:44:34 -0400

    Refactored book constructor to be class

--- script.js
+++ script.js
@@ -2,16 +2,31 @@ const myLibrary = [];
 const addBookButton = document.getElementById("add-btn");
 const closeFormButton = document.getElementById("close-form");
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
+//Original function using a constructor, restructured to be a class below
+// function Book(title, author, pages, read) {
+//   this.title = title;
+//   this.author = author;
+//   this.pages = pages;
+//   this.read = read;
+// }
 
-Book.prototype.toggleRead = function() {
-  this.read = !this.read;
-}
+// Book.prototype.toggleRead = function() {
+//   this.read = !this.read;
+// }
+
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+
+  toggleRead() {
+    this.read = !this.read;
+  }
+
+};
 
 //Functions
 //-------------------------------------------------------
